dd99f23b2e3c9b01da1e8dfee78ccf54af0ad6af
panomagic <panomagic.ua@gmail.com>
2015-08-11 20:31:44 +0300

    completely new DAO layer (+factory)2

--- dao/src/main/java/daos/GenericDAO.java
+++ dao/src/main/java/daos/GenericDAO.java
@@ -9,20 +9,20 @@ import java.util.List;
  */
 public interface GenericDAO<T, PK extends Serializable> {
     /** Creates new record and related object */
-    public T create() throws PersistException;
+    T create() throws PersistException;
 
     /** Creates new record related to object */
-    public T persist(T object) throws PersistException;
+    T persist(T object) throws PersistException;
 
     /** Returns an object with related PK 'key' or null */
-    public T getByPK(PK key) throws PersistException;
+    T getByPK(PK key) throws PersistException;
 
     /** Saves present object status to DB */
-    public void update(T object) throws PersistException;
+    void update(T object) throws PersistException;
 
     /** Deletes the record about the object from DB */
-    public void delete(T object) throws PersistException;
+    void delete(T object) throws PersistException;
 
     /** Returns object list of all related records in the DB */
-    public List<T> getAll() throws PersistException;
+    List<T> getAll() throws PersistException;
 }
