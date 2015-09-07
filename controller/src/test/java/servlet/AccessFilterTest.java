7284c39c8007270ce02e4404449ba7c824e81920
panomagic <panomagic.ua@gmail.com>
2015-09-08 02:09:10 +0300

    JUnit & Mockito 2

--- controller/src/test/java/servlet/AccessFilterTest.java
+++ controller/src/test/java/servlet/AccessFilterTest.java
@@ -3,83 +3,52 @@ package servlet;
 import beans.Role;
 import beans.User;
 import filters.AccessFilter;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AccessFilterTest extends Mockito {
 
-    /*@Mock
-    AccessFilter accessFilterMock;
+    @Mock private HttpServletRequest httpRequest;
+    @Mock private HttpServletResponse httpResponse;
+    @Mock private FilterChain filterChain;
+    @Mock private HttpSession session;
 
-    @InjectMocks
-    AccessFilter accessFilter;*/
 
-    /*@Test
-    public void testInit() throws Exception {
-        FilterConfig filterConfig = mock(FilterConfig.class);
 
-        AccessFilter accessFilter3 = mock(AccessFilter.class);
-
-        //accessFilterMock = new AccessFilter();
-
-        List<String> freeAccessUrls = new ArrayList<String>();
-        freeAccessUrls.add("/");
-        freeAccessUrls.add("/login");
-        freeAccessUrls.add("/loginfailed.jsp");
-        freeAccessUrls.add("/logout");
-        freeAccessUrls.add("/accessdenied.jsp");
-
-        List<String> clientAccessUrls = new ArrayList<String>();
-        //doNothing().when(accessFilter).init(filterConfig);
-
-        //when(accessFilter.init(filterConfig)).thenReturn(freeAccessUrls);
-
-        verify(accessFilter3).init(filterConfig);
-
-        freeAccessUrls.add("/");
-
-        //verify()
-
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void testRedirectIfLoginFailed() {
-        HttpServletResponse httpResponse = mock(HttpServletResponse.class);
-        User user = mock(User.class);
-        AccessFilter accessFilter3 = mock(AccessFilter.class);
-        //verify(accessFilter3).
-    }
-*/
-    @Test
-    public void testDoFilterIfAdmin() throws Exception {
-        HttpServletRequest httpRequest = mock(HttpServletRequest.class);
-        HttpServletResponse httpResponse = mock(HttpServletResponse.class);
-        FilterChain filterChain = mock(FilterChain.class);
-        HttpSession session = mock(HttpSession.class);
+    public void testDoFilterForAdmin() throws Exception {
+        AccessFilter accessFilter = new AccessFilter();
 
         String path = httpRequest.getRequestURI();
 
-        User loggedUser = new User();
+        User loggedUser = spy(User.class);
         loggedUser.setRole(Role.ADMINISTRATOR);
+
+        List freeAccessUrls = spy(List.class);
+        List clientAccessUrls = spy(List.class);
+        clientAccessUrls.add(path);
 
         when(httpRequest.getSession()).thenReturn(session);
 
         httpRequest.getSession().setAttribute("LOGGED_USER", loggedUser);
 
         when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(loggedUser);
-
-        List freeAccessUrls = mock(List.class);
-        List clientAccessUrls = mock(List.class);
-
-        AccessFilter accessFilter = new AccessFilter();
 
         when(freeAccessUrls.contains(path)).thenReturn(false);
 
@@ -95,17 +64,19 @@ public class AccessFilterTest extends Mockito {
     }
 
 
-    @Test
-    public void testDoFilterIfClient() throws Exception {
-        HttpServletRequest httpRequest = mock(HttpServletRequest.class);
-        HttpServletResponse httpResponse = mock(HttpServletResponse.class);
-        FilterChain filterChain = mock(FilterChain.class);
-        HttpSession session = mock(HttpSession.class);
 
-        User loggedUser = mock(User.class);
-        //when(loggedUser.equals(null)).thenReturn(false);
-        when(loggedUser.getRole()).thenReturn(Role.CLIENT);
-        //loggedUser.setRole(Role.CLIENT);
+    @Test
+    public void testDoFilterForClient() throws Exception {
+        AccessFilter accessFilter = new AccessFilter();
+
+        String path = "/login";
+
+        User loggedUser = spy(User.class);
+        loggedUser.setRole(Role.CLIENT);
+
+        List freeAccessUrls = spy(List.class);
+        List clientAccessUrls = new ArrayList<String>();
+        clientAccessUrls.add(path);
 
         when(httpRequest.getSession()).thenReturn(session);
 
@@ -113,49 +84,40 @@ public class AccessFilterTest extends Mockito {
 
         when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(loggedUser);
 
-        List freeAccessUrls = mock(List.class);
-        List clientAccessUrls = mock(List.class);
-
-        AccessFilter accessFilter = new AccessFilter();
-
-        String path = httpRequest.getRequestURI();
-
-        accessFilter.doFilter(httpRequest, httpResponse, filterChain);
+        when(loggedUser.getRole()).thenReturn(Role.CLIENT);
 
         when(freeAccessUrls.contains(path)).thenReturn(true);
 
-        when(clientAccessUrls.contains(path)).thenReturn(true);
+        //when(clientAccessUrls.contains(path)).thenReturn(true);
 
+        accessFilter.doFilter(httpRequest, httpResponse, filterChain);
 
         verify(httpResponse, never()).sendRedirect("accessdenied.jsp");
 
+        verify(httpResponse, never()).sendRedirect("loginfailed.jsp");
+
         verify(filterChain).doFilter(httpRequest, httpResponse);
-
-        //System.out.println(loggedUser = null);
-
     }
 
     @Test
     public void testDoFilterIfLoginFailed() throws Exception {
-        HttpServletRequest httpRequest = mock(HttpServletRequest.class);
-        HttpServletResponse httpResponse = mock(HttpServletResponse.class);
-        FilterChain filterChain = mock(FilterChain.class);
-        HttpSession session = mock(HttpSession.class);
+
+        AccessFilter accessFilter = new AccessFilter();
 
         String path = httpRequest.getRequestURI();
 
         User loggedUser = null;
 
+        List freeAccessUrls = spy(List.class);
+        List clientAccessUrls = spy(List.class);
+        clientAccessUrls.add(path);
+
         when(httpRequest.getSession()).thenReturn(session);
 
         httpRequest.getSession().setAttribute("LOGGED_USER", loggedUser);
 
-        when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(loggedUser);
+        when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(null);
 
-        List freeAccessUrls = mock(List.class);
-        List clientAccessUrls = mock(List.class);
-
-        AccessFilter accessFilter = new AccessFilter();
 
         when(freeAccessUrls.contains(path)).thenReturn(false);
 
