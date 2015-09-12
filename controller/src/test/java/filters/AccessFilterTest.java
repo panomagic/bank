bff70c64b2c039f18de7946a72b317ad33c34378
panomagic <panomagic.ua@gmail.com>
2015-09-13 01:41:08 +0300

    parameterized test for access filter

--- controller/src/test/java/filters/AccessFilterTest.java
+++ controller/src/test/java/filters/AccessFilterTest.java
@@ -4,19 +4,26 @@ import beans.Role;
 import beans.User;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.Collection;
 
+@RunWith(Parameterized.class)
 public class AccessFilterTest extends Mockito {
 
     @Mock private HttpServletRequest httpRequest;
     @Mock private HttpServletResponse httpResponse;
     @Mock private FilterChain filterChain;
+    @Mock private FilterConfig filterConfig;
     @Mock private HttpSession session;
     @Mock private User loggedUser;
 
@@ -25,94 +32,117 @@ public class AccessFilterTest extends Mockito {
         MockitoAnnotations.initMocks(this);
     }
 
-    @Test
-    public void testDoFilterForAdmin() throws Exception {
-        AccessFilter accessFilter = new AccessFilter();
+    static User client = new User();
+    static User admin = new User();
 
-        String path = "/admin";
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {"/", admin, true},
+            {"/", client, true},
+            {"/", null, true},
+            {"/accessdenied", admin, true},
+            {"/accessdenied", client, true},
+            {"/accessdenied", null, true},
+            {"/addaccount", admin, true},
+            {"/addaccount", client, false},
+            {"/addaccount", null, false},
+            {"/addtransaction", admin, true},
+            {"/addtransaction", client, true},
+            {"/addtransaction", null, false},
+            {"/admin", admin, true},
+            {"/admin", client, false},
+            {"/admin", null, false},
+            {"/clientinfo", admin, true},
+            {"/clientinfo", client, true},
+            {"/clientinfo", null, false},
+            {"/image", admin, true},
+            {"/image", client, true},
+            {"/image", null, false},
+            {"/loginfailed", admin, true},
+            {"/loginfailed", client, true},
+            {"/loginfailed", null, true},
+            {"/login", admin, true},
+            {"/login", client, true},
+            {"/login", null, true},
+            {"/logout", admin, true},
+            {"/logout", client, true},
+            {"/logout", null, true},
+            {"/transactionshistorybyclient", admin, true},
+            {"/transactionshistorybyclient", client, true},
+            {"/transactionshistorybyclient", null, false},
+            {"/transactionshistory", admin, true},
+            {"/transactionshistory", client, false},
+            {"/transactionshistory", null, false},
+            {"/transcurrencymismatch", admin, true},
+            {"/transcurrencymismatch", client, true},
+            {"/transcurrencymismatch", null, false},
+            {"/transoverdraft", admin, true},
+            {"/transoverdraft", client, true},
+            {"/transoverdraft", null, false},
+            {"/updatedeleteaccount", admin, true},
+            {"/updatedeleteaccount", client, false},
+            {"/updatedeleteaccount", null, false},
+            {"/upload", admin, true},
+            {"/upload", client, true},
+            {"/upload", null, false},
+        });
+    }
 
-        when(httpRequest.getSession()).thenReturn(session);
-        when(httpRequest.getRequestURI()).thenReturn(path);
-        when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(loggedUser);
-        when(loggedUser.getRole()).thenReturn(Role.ADMINISTRATOR);
+    private String path;
+    private User user;
+    private boolean access;
 
-        accessFilter.doFilter(httpRequest, httpResponse, filterChain);
-
-        verify(httpResponse, never()).sendRedirect("/accessdenied");
-        verify(httpResponse, never()).sendRedirect("/loginfailed");
-        verify(filterChain).doFilter(httpRequest, httpResponse);
+    public AccessFilterTest(String path, User user, boolean access) {
+        this.path = path;
+        this.user = user;
+        this.access = access;
     }
 
     @Test
-    public void testDoFilterForClientAvailableUrl() throws Exception {
+    public void testDoFilterAll() throws Exception {
         AccessFilter accessFilter = new AccessFilter();
-
-        String path = "/clientinfo";
-        accessFilter.clientAccessUrls.add(path);
-
-        when(httpRequest.getSession()).thenReturn(session);
-        when(httpRequest.getRequestURI()).thenReturn(path);
-        when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(loggedUser);
-        when(loggedUser.getRole()).thenReturn(Role.CLIENT);
-
-        accessFilter.doFilter(httpRequest, httpResponse, filterChain);
-
-        verify(httpResponse, never()).sendRedirect("/accessdenied");
-        verify(httpResponse, never()).sendRedirect("/loginfailed");
-        verify(filterChain).doFilter(httpRequest, httpResponse);
-    }
-
-    @Test
-    public void testDoFilterForClientFreeUrl() throws Exception {
-        AccessFilter accessFilter = new AccessFilter();
-
-        String path = "/login";
-        accessFilter.freeAccessUrls.add(path);
-
-        when(httpRequest.getSession()).thenReturn(session);
-        when(httpRequest.getRequestURI()).thenReturn(path);
-        when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(loggedUser);
-        when(loggedUser.getRole()).thenReturn(Role.CLIENT);
-
-        accessFilter.doFilter(httpRequest, httpResponse, filterChain);
-
-        verify(httpResponse, never()).sendRedirect("/accessdenied");
-        verify(httpResponse, never()).sendRedirect("/loginfailed");
-        verify(filterChain).doFilter(httpRequest, httpResponse);
-    }
-
-    @Test
-    public void testDoFilterForClientAccessDeniedUrl() throws Exception {
-        AccessFilter accessFilter = new AccessFilter();
-
-        String path = "/admin";
-        accessFilter.clientAccessUrls.add("/clientinfo");
-
-        when(httpRequest.getSession()).thenReturn(session);
-        when(httpRequest.getRequestURI()).thenReturn(path);
-        when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(loggedUser);
-        when(loggedUser.getRole()).thenReturn(Role.CLIENT);
-
-        accessFilter.doFilter(httpRequest, httpResponse, filterChain);
-
-        verify(httpResponse).sendRedirect("/accessdenied");
-        verify(httpResponse, never()).sendRedirect("/loginfailed");
-        verify(filterChain, never()).doFilter(httpRequest, httpResponse);
-    }
-
-    @Test
-    public void testDoFilterIfLoginFailed() throws Exception {
-        AccessFilter accessFilter = new AccessFilter();
-
-        String path = "/clientinfo";
 
         when(httpRequest.getSession()).thenReturn(session);
         when(httpRequest.getRequestURI()).thenReturn(path);
 
-        accessFilter.doFilter(httpRequest, httpResponse, filterChain);
+        accessFilter.init(filterConfig);
 
-        verify(httpResponse, never()).sendRedirect("/accessdenied");
-        verify(httpResponse).sendRedirect("/loginfailed");
-        verify(filterChain, never()).doFilter(httpRequest, httpResponse);
+        if ((user == admin) && access) {
+            when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(loggedUser);
+            when(loggedUser.getRole()).thenReturn(Role.ADMINISTRATOR);
+            accessFilter.doFilter(httpRequest, httpResponse, filterChain);
+            verify(httpResponse, never()).sendRedirect("/accessdenied");
+            verify(httpResponse, never()).sendRedirect("/loginfailed");
+            verify(filterChain).doFilter(httpRequest, httpResponse);
+        }
+        if ((user == client) && access) {
+            when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(loggedUser);
+            when(loggedUser.getRole()).thenReturn(Role.CLIENT);
+            accessFilter.doFilter(httpRequest, httpResponse, filterChain);
+            verify(httpResponse, never()).sendRedirect("/accessdenied");
+            verify(httpResponse, never()).sendRedirect("/loginfailed");
+            verify(filterChain).doFilter(httpRequest, httpResponse);
+        }
+        if ((user == client) && !access) {
+            when(httpRequest.getSession().getAttribute("LOGGED_USER")).thenReturn(loggedUser);
+            when(loggedUser.getRole()).thenReturn(Role.CLIENT);
+            accessFilter.doFilter(httpRequest, httpResponse, filterChain);
+            verify(httpResponse).sendRedirect("/accessdenied");
+            verify(httpResponse, never()).sendRedirect("/loginfailed");
+            verify(filterChain, never()).doFilter(httpRequest, httpResponse);
+        }
+        if ((user == null) && access) {
+            accessFilter.doFilter(httpRequest, httpResponse, filterChain);
+            verify(httpResponse, never()).sendRedirect("/accessdenied");
+            verify(httpResponse, never()).sendRedirect("/loginfailed");
+            verify(filterChain).doFilter(httpRequest, httpResponse);
+        }
+        if ((user == null) && !access) {
+            accessFilter.doFilter(httpRequest, httpResponse, filterChain);
+            verify(httpResponse, never()).sendRedirect("/accessdenied");
+            verify(httpResponse).sendRedirect("/loginfailed");
+            verify(filterChain, never()).doFilter(httpRequest, httpResponse);
+        }
     }
 }
