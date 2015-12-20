51b5cd67689e3e8fa0853dfac1e9b519e83b5223
panomagic <panomagic.ua@gmail.com>
2015-12-21 01:41:24 +0200

    Spring in controller, service and dao modules 2

---
+++ service/src/main/java/services/SpringServiceConfig.java
@@ -0,0 +1,13 @@
+package services;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+
+//@Configuration
+//@ComponentScan({"services"}) // search the package(s) for @Component classes
+public class SpringServiceConfig {
+
+}
